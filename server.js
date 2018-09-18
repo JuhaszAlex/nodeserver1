be495673adbfd8445e0f5442b1885de3497a3705
JuhaszAlex <juhaszalex95@gmail.com>
2018-09-19 01:26:53 +0200

    na bumm

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
+const db = knex({
+  	client: 'pg',
+  	connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'Nemtomaha1',
+    database : 'mydb'
+  }
+});
+
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res)=> {res.send('it is working') })
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)} )
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), ()=> {
+	console.log('app is running on port', app.get('port'));
+})
